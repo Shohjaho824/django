443dcdc8e9121b696d35152e0aca7b2d782bc580
Shohjahon824 <jumanazarovshohjahon055@example.com>
2024-12-11 14:45:33 +0500

    change

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    post = Post.objects.all()
+    context = {
+        'post': post
+    }
+    return render(request, 'index.html', context)
